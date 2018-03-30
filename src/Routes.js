162961322a7e1dd620adfc0a0e28882091d98e9d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-31 01:16:11 +0800

    Added new routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,15 +4,19 @@ import { Switch, Route } from "react-router-dom";
 import AppHomepage from "./pages/AppHomepage/AppHomepage";
 import BookCatalog from "./pages/BookCatalog/BookCatalog";
 import Dissertation from "./pages/Dissertation/Dissertation";
+import BookDetails from "./pages/BookDetails/BookDetails";
+import LogIn from "./pages/LogIn/LogIn";
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/bookCatalog/:bookName" component={BookCatalog} />
-                <Route path="/bookCatalog" component={BookCatalog} />
-                <Route path="/dissertation" component={Dissertation} />
-                <Route path="/" component={AppHomepage} />
+                <Route path="/login" component={ LogIn } />
+                <Route exact path="/book/:bookId" component={ BookDetails } />
+                <Route path="/bookCatalog/:bookName" component={ BookCatalog } />
+                <Route path="/bookCatalog" component={ BookCatalog } />
+                <Route path="/dissertation" component={ Dissertation } />
+                <Route path="/" component={ AppHomepage } />
             </Switch>
         );
     }
