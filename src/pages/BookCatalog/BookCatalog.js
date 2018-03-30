6c2560edb5caa32471f57252233a5ce350229a2e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-03-31 01:30:30 +0800

    Added rendering of books from the back end

--- src/pages/BookCatalog/BookCatalog.js
+++ src/pages/BookCatalog/BookCatalog.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Grid, Row, Col, Well } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import BookSearchForm from "../../components/BookSearchForm/BookSearchForm";
-import { Grid, Row, Col } from "react-bootstrap";
 
 class BookCatalog extends Component {
     state = {};
 
-    componentWillMount() {
+    componentDidMount() {
         let bookName = this.props.match.params.bookName;
         let url = (bookName) ? 
             'http://librarysystembackend.mybluemix.net/api/?query={Books(title:"' + bookName + '"){id title author}}' : 
@@ -16,22 +17,43 @@ class BookCatalog extends Component {
             return response.json();
         })
         .then((response) => {
-            this.setState({bookDetails: response.data});
-        })
-        setTimeout(() => {}, 100);
+            this.setState({bookDetails: response.data.Books});
+        });
+    }
+
+    // Renders the details of the given book
+    renderBookDetails(book) {
+        return (
+            // Add info if the book is reserved or borrowed
+            <Link to={"/book/" + book.id}>
+                <h4>{ book.title }</h4>
+                <h5>{ book.author }</h5>
+            </Link>
+        )
     }
 
     render() {
-        let bookName = this.props.match.params.bookName;
-        console.log(this.state.bookDetails);
+        if(this.state.bookDetails) {
+            console.log(this.state.bookDetails);
+            return (
+                <Grid>
+                    <BookSearchForm />
+                    <Row>
+                        <Col xs={12}>
+                            { this.state.bookDetails.map(this.renderBookDetails) }
+                        </Col>
+                    </Row>
+                </Grid>
+            )
+        }
+
+        // Render temporary component
         return (
             <Grid>
                 <BookSearchForm />
                 <Row>
                     <Col>
-                        <h1>
-                            { (bookName) ? bookName : "This is the book catalog page" }
-                        </h1>
+                        <h3>Loading...</h3>
                     </Col>
                 </Row>
             </Grid>
