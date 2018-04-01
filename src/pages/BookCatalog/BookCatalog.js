2a0a98dbda8f08d3d77c47f7290ba1285079901e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-01 14:28:20 +0800

    Removed Well from import statement

--- src/pages/BookCatalog/BookCatalog.js
+++ src/pages/BookCatalog/BookCatalog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Well } from "react-bootstrap";
+import { Grid, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import BookSearchForm from "../../components/BookSearchForm/BookSearchForm";
 
