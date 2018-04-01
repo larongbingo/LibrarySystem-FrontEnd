bf9d75c549ea83b8809a611dc61784050650a3d6
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-01 18:38:00 +0800

    Added a register page

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import {  } from "react-router-dom";
+import {  } from "react-bootstrap";
+
+class Register extends Component {
+    render() {
+        return (
+            <h1>This is the registration page</h1>
+        )
+    }
+}
+
+export default Register;
