2bcf2d877fd2de992bbcd9d402ae41c8458a93a1
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-02 14:09:06 +0800

    Added styling

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Form, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { 
+    Form, 
+    FormControl, 
+    ControlLabel,
+    Button,
+    Grid,
+    Row,
+    Col
+} from "react-bootstrap";
 
 class AppLogin extends Component {
     constructor(props, context) {
@@ -28,22 +36,31 @@ class AppLogin extends Component {
     render() {
         return (
             <div>
-                <Form>
-                    <FormControl 
-                        type="text"
-                        value={this.state.username}
-                        placeholder="Username"
-                        onChange={this.handleUsernameChange}
-                    />
-                    <FormControl 
-                        type="password"
-                        value={this.state.password}
-                        placeholder="Password"
-                        onChange={this.handlePasswordChange}
-                    />
-                    <Button>Log In</Button>
-                </Form>
-                <Link to="/register">Don't have an account? Register with your School ID</Link>
+                <Grid>
+                    <Row>
+                        <Col mdOffset={2} md={8} xs={12}>
+                            <Form>
+                                <ControlLabel>Username</ControlLabel>
+                                <FormControl 
+                                    type="text"
+                                    value={this.state.username}
+                                    placeholder="Username"
+                                    onChange={this.handleUsernameChange}
+                                />
+
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl 
+                                    type="password"
+                                    value={this.state.password}
+                                    placeholder="Password"
+                                    onChange={this.handlePasswordChange}
+                                />
+                                <Button bsStyle="success">Log In</Button>
+                            </Form>
+                            <Link to="/register">Don't have an account? Register with your School ID</Link>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         )
     }
