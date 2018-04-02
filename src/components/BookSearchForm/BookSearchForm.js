20b1c7d7ff2a462c5260518c25a6ccadb2672390
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-02 14:09:28 +0800

    Added window.location.replace at enter key event

--- src/components/BookSearchForm/BookSearchForm.js
+++ src/components/BookSearchForm/BookSearchForm.js
@@ -30,9 +30,7 @@ class BookSearchForm extends Component {
         if(e.key === "Enter") {
             // At Keypress of enter, redirect to /bookCatalog/ + this.state.value
             e.preventDefault();
-            return (
-                <Redirect to={"/bookCatalog/" + this.state.value} />
-            )
+            window.location.replace("/bookCatalog/" + this.state.value);
         }
     }
 
@@ -40,22 +38,24 @@ class BookSearchForm extends Component {
         return (
             <Form horizontal onKeyPress={this.handleEnterKeyPress}>
                 <FormGroup controlId="searchBook">
-                    <Col md={8}>
-                        <FormControl 
-                            type="text"
-                            value={this.state.value}
-                            placeholder="Search Book here. Enter a title. Ex. Discrete Mathematics"
-                            onChange={this.handleChange}
-                        />
-                        <FormControl.Feedback />
-                    </Col>
-                    
-                    <Col md={4}>
-                        <Button 
-                            href={"/bookCatalog/" + this.state.value} 
-                            type="submit" 
-                            bsStyle="success"
-                        >Search Book</Button>
+                    <Col mdOffset={1} xs={12}>
+                        <Col md={8}>
+                            <FormControl 
+                                type="text"
+                                value={this.state.value}
+                                placeholder="Search Book here. Enter a title. Ex. Discrete Mathematics"
+                                onChange={this.handleChange}
+                            />
+                            <FormControl.Feedback />
+                        </Col>
+                        
+                        <Col md={4}>
+                            <Button 
+                                href={"/bookCatalog/" + this.state.value} 
+                                type="submit" 
+                                bsStyle="success"
+                            >Search Book</Button>
+                        </Col>
                     </Col>
                 </FormGroup>
             </Form>
