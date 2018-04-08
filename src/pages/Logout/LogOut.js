e62b701b2e40ad92767ebafa04b6f4bbfca38989
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-08 20:20:05 +0800

    Added API Request to remove token at backend

--- src/pages/Logout/LogOut.js
+++ src/pages/Logout/LogOut.js
@@ -4,7 +4,11 @@ import { Redirect } from "react-router-dom";
 class LogOut extends Component {
     constructor(props, context) {
         super(props, context);
-
+        
+        // Remove the token at the backend
+        fetch(`https://librarysystembackend.mybluemix.net/api?query=mutation+{logOut(token:"${localStorage.getItem("hash")}")}`,{method: "POST"})
+        
+        // Remove the locally stored token
         localStorage.removeItem("hash");
     }
     
