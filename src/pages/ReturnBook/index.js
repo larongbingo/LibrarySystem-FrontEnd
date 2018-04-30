70a8549c0dfbe39744af2e1b1af1d1acf55db48e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-04-30 18:18:17 +0800

    Added Return Book Page

---
+++ src/pages/ReturnBook/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class ReturnBook extends Component {
+    render() {
+        return (
+            <h1>This is the return book page</h1>
+        )
+    }
+}
+
+export default ReturnBook;
